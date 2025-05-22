6516a59cd8231f92da084aba5d2c0eb6c8d2a746
Luan Custódio <luancustodio@sou.faccat.br>
2025-05-21 23:47:13 -0300

    Create backtracking.py

---
+++ atividade-recursivos/backtracking.py
@@ -0,0 +1,19 @@
+labirinto = [
+    [0, 1, 0],
+    [0, 0, 0],
+    [1, 1, 0]
+]
+def resolver(x, y):
+    if x < 0 or y < 0 or x >= 3 or y >= 3: 
+        return False
+    if labirinto[x][y] != 0:
+        return False
+    if (x, y) == (2, 2):
+        print("Chegou à saída!")
+        return True
+    labirinto[x][y] = 2
+    if (resolver(x+1, y) or resolver(x-1, y) or
+        resolver(x, y+1) or resolver(x, y-1)):
+        return True
+    return False
+resolver(0, 0)
